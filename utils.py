623a076362c06313e4ca38e782f2759ec63daa26
Dev_botz <deveshpareek001@gmail.com>
2025-08-09 22:57:18 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -589,33 +589,33 @@ async def get_cap(settings, remaining_seconds, files, query, total_results, sear
                     url=imdb['url'],
                     **locals()
                 )
-                for idx, file in enumerate(files, start=offset+1):
-                        cap += (
-                            f"<b>{idx}. "
-                            f"<a href='https://telegram.me/{temp.U_NAME}"
-                            f"?start=file_{query.message.chat.id}_{file.file_id}'>"
-                            f"[{get_size(file.file_size)}] "
-                            f"{clean_filename(file.file_name)}\n\n"
-                            f"</a></b>"
+                    for idx, file in enumerate(files, start=offset+1):
+                             cap += (
+                                f"<b>{idx}. "
+                                f"<a href='https://telegram.me/{temp.U_NAME}"
+                                f"?start=file_{query.message.chat.id}_{file.file_id}'>"
+                                f"[{get_size(file.file_size)}] "
+                                f"{clean_filename(file.file_name)}\n\n"
+                                f"</a></b>"
+                            )
+                    else:
+                        cap = (
+                            f"<b>🏷 ᴛɪᴛʟᴇ : <code>{search}</code>\n"
+                            f"🧱 ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n"
+                            f"⏰ ʀᴇsᴜʟᴛ ɪɴ : <code>{remaining_seconds} Sᴇᴄᴏɴᴅs</code>\n\n"
+                            f"📝 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {query.from_user.mention}\n"
+                            f"⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ :⚡ {query.message.chat.title}\n</b>"
                         )
-                else:
-                    cap = (
-                        f"<b>🏷 ᴛɪᴛʟᴇ : <code>{search}</code>\n"
-                        f"🧱 ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n"
-                        f"⏰ ʀᴇsᴜʟᴛ ɪɴ : <code>{remaining_seconds} Sᴇᴄᴏɴᴅs</code>\n\n"
-                        f"📝 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {query.from_user.mention}\n"
-                        f"⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ :⚡ {query.message.chat.title}\n</b>"
-                    )
-                    cap += "\n\n🧾 <u>Your Requested Files Are Here</u> 👇 👇\n\n</b>"
-                    for idx, file in enumerate(files, start=offset + 1):
-                        cap += (
-                            f"<b>{idx}. "
-                            f"<a href='https://telegram.me/{temp.U_NAME}"
-                            f"?start=file_{query.message.chat.id}_{file.file_id}'>"
-                            f"[{get_size(file.file_size)}] "
-                            f"{clean_filename(file.file_name)}\n\n"
-                            f"</a></b>"
-                        )
+                        cap += "\n\n🧾 <u>Your Requested Files Are Here</u> 👇 👇\n\n</b>"
+                        for idx, file in enumerate(files, start=offset + 1):
+                            cap += (
+                                f"<b>{idx}. "
+                                f"<a href='https://telegram.me/{temp.U_NAME}"
+                                f"?start=file_{query.message.chat.id}_{file.file_id}'>"
+                                f"[{get_size(file.file_size)}] "
+                                f"{clean_filename(file.file_name)}\n\n"
+                                f"</a></b>"
+                            )
 
         else:
             cap = (
