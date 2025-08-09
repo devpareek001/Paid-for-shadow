5dffbc0a8ef2b203352f63f84f5ecb1ab3139d2d
Dev_botz <deveshpareek001@gmail.com>
2025-08-09 22:42:39 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -617,26 +617,26 @@ async def get_cap(settings, remaining_seconds, files, query, total_results, sear
                             f"</a></b>"
                         )
 
-        else:
-            cap = (
-                f"<b>🏷 ᴛɪᴛʟᴇ : <code>{search}</code>\n"
-                f"🧱 ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n\n"
-                f"📝 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {query.from_user.mention}\n"
-                f"⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : ⚡ {query.message.chat.title or temp.B_LINK or 'ᴅʀᴇᴀᴍxʙᴏᴛᴢ'}\n</b>"
-            )
-            cap += "\n\n🧾 <u>Your Requested Files Are Here</u> 👇\n\n</b>"
-            for idx, file in enumerate(files, start=offset):
-                        cap += (
-                            f"<b>{idx}. "
-                            f"<a href='https://telegram.me/{temp.U_NAME}"
-                            f"?start=file_{query.message.chat.id}_{file.file_id}'>"
-                            f"[{get_size(file.file_size)}] "
-                            f"{clean_filename(file.file_name)}\n\n"
-                            f"</a></b>"
-                        )
-        return cap
+                else:
+                    cap = (
+                        f"<b>🏷 ᴛɪᴛʟᴇ : <code>{search}</code>\n"
+                        f"🧱 ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n\n"
+                        f"📝 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {query.from_user.mention}\n"
+                        f"⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : ⚡ {query.message.chat.title or temp.B_LINK or 'ᴅʀᴇᴀᴍxʙᴏᴛᴢ'}\n</b>"
+                    )
+                    cap += "\n\n🧾 <u>Your Requested Files Are Here</u> 👇\n\n</b>"
+                    for idx, file in enumerate(files, start=offset):
+                                cap += (
+                                    f"<b>{idx}. "
+                                    f"<a href='https://telegram.me/{temp.U_NAME}"
+                                    f"?start=file_{query.message.chat.id}_{file.file_id}'>"
+                                    f"[{get_size(file.file_size)}] "
+                                    f"{clean_filename(file.file_name)}\n\n"
+                                    f"</a></b>"
+                                )
+                return cap
 
-async def group_setting_buttons(grp_id):
+        async def group_setting_buttons(grp_id):
     
     settings = await get_settings(grp_id)
     buttons = [[
