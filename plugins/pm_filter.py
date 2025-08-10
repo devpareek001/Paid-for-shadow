045ac258f2cd0cf7cd148f282f515fd8339c1139
Dev_botz <deveshpareek001@gmail.com>
2025-08-10 11:35:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2142,8 +2142,7 @@ async def auto_filter(client, msg, spoll=False):
 
             for idx, file in enumerate(files, start=1):
                 cap += f"<b>\n{idx}. <a href='https://telegram.me/{temp.U_NAME}?start=file_{message.chat.id}_{file.file_id}'>[{get_size(file.file_size)}] {clean_filename(file.file_name)}\n</a></b>"
-    if imdb and imdb.get('poster'):
-    pass  
+    if imdb and imdb.get('poster'):  
         try:
             hehe = await m.edit_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn), parse_mode=enums.ParseMode.HTML)
             try:
